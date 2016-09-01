c2ecee08a7c5d29aba3ef49cce404330144c0b3b
MaxRussell42 <maxarussell@gmail.com>
2016-09-01 09:49:26 -0700

    9/1/16 Added to Calculate

---
+++ TempConverter/src/tempConverter.java
@@ -0,0 +1,12 @@
+//Max Russell 8/30/16 Farhenheit to Celsius converter
+public class tempConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int farhenheit = 42;
+		int celsius = ((farhenheit- 32)* 5/9);
+		System.out.println("This is the temperature in Celsius");
+		System.out.println(celsius);
+	}
+
+}
